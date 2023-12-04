c9d7ea2915b1cabf10f8a0d65f138dce4b859150
Steallight <steallight.lp@gmail.com>
2023-12-04 20:09:10 +0100

    added Dependencies

--- src/main/java/de/steallight/discordwhitelist/DiscordWhitelist.java
+++ src/main/java/de/steallight/discordwhitelist/DiscordWhitelist.java
@@ -1,11 +1,33 @@
 package de.steallight.discordwhitelist;
 
+import net.dv8tion.jda.api.JDA;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class DiscordWhitelist extends JavaPlugin {
 
+    private JDA jda;
+
+    private static DiscordWhitelist INSTANCE;
+
+    public void config(){
+        File c = new File("plugins/DiscordWhitelist", "config.yml");
+        FileConfiguration cfg = YamlConfiguration.loadConfiguration(c);
+        if (!c.exists()){
+            cfg.options().copyDefaults(true);
+            cfg.addDefault("BOT_TOKEN", "");
+
+        }
+    }
+
     @Override
     public void onEnable() {
+        config();
+        saveDefaultConfig();
+    INSTANCE = this;
         // Plugin startup logic
 
     }
@@ -13,5 +35,9 @@ public final class DiscordWhitelist extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static DiscordWhitelist getPlugin(){
+        return INSTANCE;
     }
 }
