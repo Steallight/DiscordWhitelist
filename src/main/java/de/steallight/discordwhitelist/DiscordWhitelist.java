cf45ea63d92654e179c23935c333d3ea99e61dd9
Steallight <steallight.lp@gmail.com>
2023-12-04 20:04:31 +0100

    upload

---
+++ src/main/java/de/steallight/discordwhitelist/DiscordWhitelist.java
@@ -0,0 +1,17 @@
+package de.steallight.discordwhitelist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordWhitelist extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
