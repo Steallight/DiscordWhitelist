1740d553db0417e1427930514d2347436be94c70
Luca <steallight.lp@gmail.com>
2024-03-30 11:59:47 +0100

    Fixed Security Functions

--- src/main/java/de/steallight/discordwhitelist/listener/ButtonHandler.java
+++ src/main/java/de/steallight/discordwhitelist/listener/ButtonHandler.java
@@ -3,6 +3,7 @@ package de.steallight.discordwhitelist.listener;
 import de.steallight.discordwhitelist.DiscordWhitelist;
 import de.steallight.discordwhitelist.utils.LiteSQL;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
@@ -27,6 +28,7 @@ public class ButtonHandler extends ListenerAdapter {
 
     String logChannelID = DiscordWhitelist.getPlugin().getConfig().getString("LOG_CHANNEL_ID");
 
+    EmbedBuilder eb = new EmbedBuilder();
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent e) {
         if (e.getComponentId().startsWith("request-accept")) {
@@ -51,7 +53,7 @@ public class ButtonHandler extends ListenerAdapter {
             if (!player.isWhitelisted()) {
                 try {
 
-                    EmbedBuilder eb = new EmbedBuilder();
+
 
                     eb
                             .setTitle("Whitelist-Log")
@@ -70,7 +72,6 @@ public class ButtonHandler extends ListenerAdapter {
                     insertMcBinder(DiscordWhitelist.getPlugin().database, UserId, minecraftname);
 
 
-
                     tc.sendMessageEmbeds(eb.build()).queue();
                     e.reply("Der User wurde gewhitelisted!").setEphemeral(true).queue();
                     e.getMessage().delete().queue();
@@ -83,28 +84,55 @@ public class ButtonHandler extends ListenerAdapter {
             }
             //Identify Kick Button Handling
         } else if (e.getComponentId().startsWith("identifyKick")) {
-            String userId = e.getComponentId().substring(13);
-            e.getGuild().kick(e.getGuild().getMemberById(userId)).queue();
+            if (e.getMember().hasPermission(Permission.KICK_MEMBERS)) {
+                String userId = e.getComponentId().substring(13);
+
+                e.getGuild().kick(e.getGuild().getMemberById(userId)).queue();
+
+                eb
+                        .setTitle("Der User wurde gekickt!")
+                        .setColor(Color.GREEN);
+                e.replyEmbeds(eb.build()).setEphemeral(true).queue();
+                writeLog(e.getGuild().getMemberById(userId).getNickname(), Color.RED, "gekickt von " + e.getMember(),e);
+            }else {
+                e.reply("Dazu hast du keine Rechte").setEphemeral(true).queue();
+            }
 
             //Identify Ban Button Handling
         } else if (e.getComponentId().startsWith("identifyBan")) {
-            String userId = e.getComponentId().substring(12);
-            String ouputData = e.getComponentId();
-            int startIndex = ouputData.indexOf("[");
-            int endIndex = ouputData.indexOf("]");
-            minecraftname = e.getComponentId().substring(startIndex + 1, endIndex);
+            if (e.getMember().hasPermission(Permission.BAN_MEMBERS)) {
+                String userId = e.getComponentId().substring(12);
+                String ouputData = e.getComponentId();
+                int startIndex = ouputData.indexOf("[");
+                int endIndex = ouputData.indexOf("]");
+                minecraftname = e.getComponentId().substring(startIndex + 1, endIndex);
 
-            OfflinePlayer player = Bukkit.getOfflinePlayer(minecraftname);
-            e.getGuild().ban(e.getGuild().getMemberById(userId), 10, TimeUnit.DAYS).queue();
-            player.ban("", Instant.ofEpochSecond(0),"").save();
+                OfflinePlayer player = Bukkit.getOfflinePlayer(minecraftname);
+                e.getGuild().ban(e.getGuild().getMemberById(userId), 10, TimeUnit.DAYS).queue();
+                player.ban("", Instant.ofEpochSecond(0), "").save();
 
+                eb
+                        .setTitle("Der User wurde gebannt")
+                        .setColor(Color.GREEN);
+                e.replyEmbeds(eb.build()).setEphemeral(true).queue();
+                writeLog(minecraftname + " wurde gebannt", Color.RED, "gebannt von " + e.getMember().getNickname(), e);
+            }else {
+
+                e.reply("Du hast dazu keine Rechte").queue();
+            }
 
         } else if (e.getComponentId().startsWith("identifyDewhitelist")) {
 
             minecraftname = e.getComponentId().substring(20);
+            OfflinePlayer player = Bukkit.getOfflinePlayer(minecraftname);
 
-            new DeWhitlistPlayer().runTask(DiscordWhitelist.getPlugin());
-            e.reply("Der User wurde von der Whitelist entfernt").setEphemeral(true).queue();
+            if (player.isWhitelisted()) {
+                new DeWhitlistPlayer().runTask(DiscordWhitelist.getPlugin());
+                e.reply("Der User wurde von der Whitelist entfernt").setEphemeral(true).queue();
+                writeLog(minecraftname + " von der Whitelist entfernt", Color.green, "entfernt von " + e.getMember().getNickname(), e);
+            }else {
+                e.reply("Der User steht nicht auf der Whitelist").queue();
+            }
         }
     }
 
@@ -117,7 +145,7 @@ public class ButtonHandler extends ListenerAdapter {
         }
     }
 
-    public class DeWhitlistPlayer extends BukkitRunnable{
+    public class DeWhitlistPlayer extends BukkitRunnable {
         @Override
         public void run() {
             OfflinePlayer player = Bukkit.getOfflinePlayer(minecraftname);
@@ -125,7 +153,6 @@ public class ButtonHandler extends ListenerAdapter {
             player.setWhitelisted(false);
         }
     }
-
 
 
     public void insertMcBinder(LiteSQL sql, String UserID, String minecraftusername) throws SQLException {
@@ -136,6 +163,18 @@ public class ButtonHandler extends ListenerAdapter {
         stmtInsertMcBinder.executeUpdate();
         stmtInsertMcBinder.close();
         con.close();
+    }
+
+    public void writeLog(String Title, Color color, String footer, ButtonInteractionEvent e){
+        eb
+                .setTitle(Title)
+                .setColor(color)
+                .setFooter(footer);
+
+        e.getGuild().getTextChannelById(logChannelID).sendMessageEmbeds(eb.build()).queue();
+
+
+
     }
 
 
